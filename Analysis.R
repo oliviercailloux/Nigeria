8664674f4fee989ea9892c9c6bb413a0f270f50e
Olivier Cailloux <olivier.cailloux@gmail.com>
2025-04-18 18:05:05 +0200

    model

--- Analysis.R
+++ Analysis.R
@@ -7,26 +7,35 @@ acled_raw <- as_tibble(read.csv("ACLED.csv"))
 acled <- acled_raw |> mutate(year = year(event_date))
 y_events <- acled |>
   group_by(year) |>
-  summarize(nb_events_y = n(), nb_fatalities_y = sum(fatalities))
+  summarize(nb_events_y = n(), nb_fatalities_y = sum(fatalities)) |>
+  mutate(log_nb_events_y = log(nb_events_y), log_nb_fatalities_y = log(nb_fatalities_y))
 stopifnot(sum(acled$fatalities) == sum(y_events$nb_fatalities_y))
-y_events_ext <- y_events
+by_year <- y_events
 for (i in 1:3) {
-  y_events_ext <- y_events_ext |> mutate(nb_events_y_minus_1 = lag(nb_events_y, 1))
-  d[[glue("nb_events_y_minus_", i)]] <- y_events |>
-    pull(nb_events_y) |>
-    lag(i)
-  d[[glue("nb_fatalities_y_minus_", i)]] <- y_events |>
-    pull(nb_fatalities_y) |>
-    lag(i)
+  by_year <- by_year |>
+    mutate(
+      "log_nb_events_y_minus_{i}" := lag(log_nb_events_y, i),
+      "log_nb_fatalities_y_minus_{i}" := lag(log_nb_fatalities_y, i)
+    )
 }
-y_events_ext <- y_events |> mutate(!!!d)
 
-wb <- as_tibble(read.csv("WB year.csv"))
+wb <- as_tibble(read.csv("WB year.csv")) |> rename(year = Year)
 wb_descr <- as_tibble(read.csv("WB descr.csv"))
-gdp <- acled$gdp
+log_gdp <- log(wb$NY.GDP.PCAP.KD)
+diffs <- c(NA, diff(log_gdp, lag = 1))
+wb_ext <- wb |> mutate(delta_log_gdp = diffs) |> rename(gdp = NY.GDP.PCAP.KD, infl = NY.GDP.DEFL.KD.ZG.AD, fdi = BM.KLT.DINV.WD.GD.ZS, trade = TG.VAL.TOTL.GD.ZS, military_exp = MS.MIL.XPND.ZS, export = TX.VAL.MRCH.XD.WD)
 
-model <- lm(gdp ~ a + b, data = data)
+by_year <- by_year |>
+  inner_join(wb_ext, by = "year")
 
+model <- lm(delta_log_gdp ~ log_nb_events_y + log_nb_fatalities_y + infl + fdi + trade + military_exp + export, by_year)
+summary(model)
+# delta_log_gdp = log gdp year y2 - log gdp year y1 = log(gdp year y2 / gdp year y1)
+# delta_log_gdp = a + b1 * nb_events_y + b2 * nb_fatalities_y means that
+# gdp year y2 = gdp year y1 * e^(a + b1 * nb_events_y + b2 * nb_fatalities_y)
+# gdp year y2 = gdp year y1 * e^a * e^(b1 * nb_events_y + b2 * nb_fatalities_y)
+# gdp year y2 = gdp year y1 * e^a * e^(b1 * nb_events_y) * e^(b2 * nb_fatalities_y)
+# x2 = x1 × e^dlog_gdp
 # f_by_e <- t |>
 #   group_by(event_type) |>
 #   summarize(sum(fatalities))
